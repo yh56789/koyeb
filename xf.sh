baad4f0b218f9394b5ba0e1f3d7681cd5de255f4
yh56789 <yh567890@gmail.com>
2022-03-29 10:21:38 +0800

    Add files via upload

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "f1770938-24cf-4385-bcc7-31a0b06189b0",
                         "alterId": 0
                     }
                 ],
